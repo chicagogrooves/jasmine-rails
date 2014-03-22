8f4f03d79bf36001c5c9bcd405190bf033c7397c
Dean Radcliffe <dean@bassdrum>
2014-03-21 22:40:53 -0500

    Most specs pass at http://localhost:3333/specs controller
    Note: still not working yet in PhantomJS

--- app/controllers/jasmine_rails/spec_runner_controller.rb
+++ app/controllers/jasmine_rails/spec_runner_controller.rb
@@ -2,6 +2,14 @@ module JasmineRails
   class SpecRunnerController < JasmineRails::ApplicationController
     def index
       JasmineRails.reload_jasmine_config
+      @require_config_json = JSON.dump(Rails.application.config.requirejs.run_config).html_safe
+
+      prereqs =  Rails.application.config.requirejs.user_config['load_with_specs']
+      spec_list = Dir["#{Rails.root}/spec/javascripts/**/*.coffee"].map do |x|
+        x.sub("#{Rails.root}/spec/javascripts/","").sub(/\.coffee/,"")
+      end 
+
+      @spec_modules_json = JSON.dump(prereqs+spec_list).html_safe
     end
   end
 end
